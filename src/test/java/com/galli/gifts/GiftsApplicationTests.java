13d7161df2a84c8eb84e36c5f6b5822974426f7d
gustavogalli <gustavosgalli@gmail.com>
2025-04-27 21:10:00 -0300

    [feat] Adding main files

---
+++ src/test/java/com/galli/gifts/GiftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galli.gifts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
